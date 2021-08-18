d01e422b55b4b565d4c5cd04f61ed3959385bd38
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-08-18 23:02:54 +0300

    Update nuxt.config.js

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ssr: false,
   target: 'static',
   head: {
-    title: 'enab-nuxt',
+    title: 'enab-nuxt', 
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0' },
