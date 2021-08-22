561d5a50ddf423b4bcb0428d9f35bc2f18b4fc89
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-08-22 19:10:07 +0300

    Update BrowserWinHandler.js

--- src/main/BrowserWinHandler.js
+++ src/main/BrowserWinHandler.js
@@ -33,7 +33,7 @@ export default class BrowserWinHandler {
   _createInstance () {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
+    // Some APIs can only be used after this event occurs. 
     if (app.isReady()) this._create()
     else {
       app.once('ready', async () => {
