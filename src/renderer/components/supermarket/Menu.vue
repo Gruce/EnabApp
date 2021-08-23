8962045e29ae99b6eebee318a53912509a5a5f91
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-08-23 20:03:04 +0300

    Update Menu.vue

--- src/renderer/components/supermarket/Menu.vue
+++ src/renderer/components/supermarket/Menu.vue
@@ -5,7 +5,7 @@
       <div v-if="updateProgress.percent" class="row">
       <!-- Downloading Update -->
         <div class="progress-wrapper px-0" v-if="updateProgress.percent !== 100">
-            <span class="progress-percentage text-light">جاري التحديث</span>
+            <span class="progress-percentage text-light">جاري التحديث...</span>
             <span class="progress-label text-light">{{ Math.round(updateProgress.percent * 100)/100 }}%</span>
             <div class="progress mt-2 p-0" style="height: 10px;">
                 <div class="progress-bar bg-dark" role="progressbar" :aria-valuenow="updateProgress.percent" aria-valuemin="0" aria-valuemax="100" :style="'width: '+updateProgress.percent+'%;'"></div>
