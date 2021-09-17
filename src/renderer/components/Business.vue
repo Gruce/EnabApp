1af35e99008cbee44a654854c2100ec344e44f35
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-09-17 19:56:18 +0300

    multiple businesses

--- src/renderer/components/Business.vue
+++ src/renderer/components/Business.vue
@@ -1,52 +1,167 @@
 <template>
   <div>
-    <div @click="initializeService()" class="card shadow-lg bg r-2 hover-shadow-lg hover-translate-y-n10 z-index-100 pointer">
+    <div @click="initializeService()" class="card mb-1 shadow-lg bg r-2 hover-shadow-lg hover-translate-y-n10 z-index-100 pointer">
       <div class="card-body px-lg-5 pt-4 text-center h-100">
-        <div v-if="!$auth.user[name]" class="position-absolute top-0 end-0">
-          <span class="badge badge-danger mr-1 r-2">
-            لاتمتلك هذه الخدمة
-          </span>
+        <div v-if="loading">
+          <UtilitiesLoading />
         </div>
-        <div class="icon icon-xl icon-shape rounded-circle t-3">
-          <i class="fa-5x text-light" :class="icon ? 'fas fa-shopping-cart' : 'fas fa-briefcase'"></i>
+
+        <div v-else>
+          <div v-if="!$auth.user[name]" class="position-absolute top-0 end-0">
+            <span class="badge badge-danger mr-1 r-2">
+              لاتمتلك هذه الخدمة
+            </span>
+          </div>
+
+          <div class="icon icon-xl icon-shape rounded-circle t-3">
+            <i class="fa-5x text-light" :class="icon ? 'fas fa-shopping-cart' : 'fas fa-briefcase'"></i>
+          </div>
+          <div class="mt-4 fw-bold fs-4 text-light mb-0">
+            <span v-if="$auth.user[name]">
+              {{ $auth.user[name].name }}
+            </span>
+            <span v-else>
+              {{ title }}
+            </span>
+          </div>
+          <hr class="divider divider-fade my-3">
+          <div class="fs-6 mb-0 text-muted">
+            <span v-if="$auth.user[name]">
+              {{ user_type($auth.user[name].pivot.user_type) }}
+            </span>
+            <span v-else>
+              {{ sub_title }}
+            </span>
+          </div>
         </div>
-        <div class="mt-4 fw-bold fs-4 text-light mb-0">{{ title }}</div>
-        <hr class="divider divider-fade my-3">
-        <div class="fs-6 mb-0 text-muted">{{ sub_title }}</div>
       </div>
+    </div>
+    <div>
+      <c-menu>
+        <c-menu-button class="bg r-2 w-100" right-icon="chevron-down">
+          تغيير الـ{{ title }}
+        </c-menu-button>
+        <c-menu-list class="bg r-2 w-100">
+          <c-menu-item v-for="(business, i) in all" :key="business.id" @click="changeBusiness(business.id)">
+            <span class="badge t-1 ml-1">{{++i}}</span>
+            {{ business.name }}
+          </c-menu-item>
+
+          <hr class="bg-secondary my-2" />
+
+          <c-menu-item @click="newBusiness()">
+            <span class="badge t-1 ml-1">
+              <i class="fas fa-plus"></i>
+            </span>
+            {{ title }} إضافي
+          </c-menu-item>
+        </c-menu-list>
+      </c-menu>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["title", "sub_title", "new_service", "link", "name", "icon"],
+  props: ["title", "sub_title", "new_service", "link", "name", "icon", "all"],
+  data() {
+    return {
+      loading: false,
+    };
+  },
   methods: {
     async initializeService() {
-      if (this.$auth.user[this.name]){
+      if (this.$auth.user[this.name]) {
         this.$router.push("/" + this.name);
       } else {
-        await this.$axios
-          .post(
-            "/api/business",
-            { business: this.name },
-            {
-              withCredentials: true,
-            }
-          )
-          .then((response) => {
-            this.$toast({
-              title: "تم الإنشاء بنجاح!",
-              description: "تم إنشاء منصة العمل الخاصة بك بنجاح!",
-              status: "success",
-              duration: 3000,
-            });
-  
-            this.$router.push("/" + this.name);
-          })
-          .catch((error) => {
-            console.log(error);
+        this.sendCreateBusiness();
+      }
+    },
+    async changeBusiness(id) {
+      await this.$axios
+        .post(
+          "/api/" + this.name + "/select",
+          { id: id },
+          {
+            withCredentials: true,
+          }
+        )
+        .then(async (response) => {
+          await this.$auth.fetchUser();
+
+          this.$toast({
+            title: "تم التغيير بنجاح",
+            description: "تم تغيير العمل",
+            status: "success",
+            duration: 2000,
           });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    async newBusiness() {
+      let businesses = this.all.filter((x) => x.pivot.user_type == "admin");
+
+      if (businesses.length > 0) {
+        if (this.$auth.user.points >= 12000)
+          this.$dialog.confirm("سيتم إستقطاع 12,000 نقطة").then(async () => {
+            this.sendCreateBusiness(true);
+          });
+        else
+          this.$toast({
+            title: "عدد النقاط غير كافي",
+            description: "يجب شحن نقاط أكثر في حسابك",
+            status: "error",
+            duration: 5000,
+          });
+      } else {
+        this.sendCreateBusiness();
+      }
+    },
+
+    async sendCreateBusiness(newBusiness = false) {
+      await this.$axios
+        .post(
+          "/api/business",
+          { business: this.name, new: newBusiness },
+          {
+            withCredentials: true,
+          }
+        )
+        .then(async (response) => {
+          // Selecting Business
+          await this.$axios
+            .post(
+              "/api/" + this.name + "/select",
+              { id: response.data.id },
+              { withCredentials: true }
+            )
+            .then(async (response) => {
+              this.$auth.fetchUser();
+
+              this.$toast({
+                title: "تم الإنشاء بنجاح!",
+                description: "تم إنشاء منصة العمل الخاصة بك بنجاح!",
+                status: "success",
+                duration: 3000,
+              });
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    user_type(value) {
+      switch (value) {
+        case "admin":
+          return "المدير";
+        case "staff":
+          return "طاقم العمل";
+        default:
+          return "غير معروف";
       }
     },
   },
--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="container mt-6">
-    <div class="row t-1 r-2 b-1 py-3">
-      <div class="col-6">
+    <div class="row t-1 r-2 b-1 p-3">
+      <nuxt-link to="/" class="col-6 p-3 clickable">
         <div class="row align-items-center">
           <div class="col-auto">
-            <img alt="Image placeholder" :src="$auth.user.profile_photo_url" class="avatar avatar-xl r-2 mr-3">
+            <img alt="Image placeholder" :src="$auth.user.profile_photo_url" class="avatar avatar-xl r-2">
           </div>
           <div class="col ml-n3 ml-md-n2">
             <span class="fs-4 text-light">
               {{ $auth.user.name }}
             </span>
-            <span class="d-block">
+            <span class="d-block text-light">
               {{ $auth.user.email }}
             </span>
           </div>
         </div>
-      </div>
-      <div class="col-6">
+      </nuxt-link>
+      <nuxt-link to="/credits" class="col-6 p-3 clickable">
         <div class="row align-items-center">
           <div class="col ml-n3 ml-md-n2 text-left">
             <span class="fs-4 text-light">
               رصيد نقاطك
             </span>
-            <span class="d-block">
+            <span class="d-block text-light">
               {{ $auth.user.points }}
             </span>
           </div>
           <div class="col-auto">
-            <div class="avatar avatar-xl bg-light r-2 ml-3">
+            <div class="avatar avatar-xl bg-light r-2">
               <i class="fas fa-credit-card fa-2x text-dark"></i>
             </div>
           </div>
         </div>
-      </div>
+      </nuxt-link>
     </div>
 
     <div class="row d-flex justify-content-center mt-3 b-1 r-2 p-4 show-scroll businesses">
@@ -42,7 +42,7 @@
         </span>
       </div> -->
       <div v-for="business in $auth.user.businesses" :key="business.name" class="col-md-4 px-lg-4">
-        <Business :link="'/'+business.name" :name="business.name" :title="business.title" :sub_title="business.subtitle" :icon="business.icon" :new_service="true" />
+        <Business :link="'/'+business.name" :name="business.name" :title="business.title" :sub_title="business.subtitle" :icon="business.icon" :all="business.all" :new_service="true" />
       </div>
     </div>
   </div>
@@ -70,5 +70,13 @@ h1 {
 .businesses {
   min-height: calc(100vh - 21rem) !important;
   max-height: calc(100vh - 21rem) !important;
+}
+
+.clickable{
+  transition: .2s ease-out;
+  border-radius: $r-2;
+  &:hover{
+    background: $tb-2;
+  }
 }
 </style>
