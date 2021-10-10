b05f25c5ab627a154a927e2600f1b531de7925d8
Hassan Alkhalidi <gruceing@gmail.com>
2021-10-10 15:35:12 +0300

    Update DailyGoals.vue

--- src/renderer/components/supermarket/Home/Performance/DailyGoals.vue
+++ src/renderer/components/supermarket/Home/Performance/DailyGoals.vue
@@ -7,18 +7,20 @@
         </div>
       </div>
       <div class="card-body p-2">
-        <div v-for="result in results" :key="result.title" class="d-flex align-items-center goal p-3 r-2">
-          <div class="icon icon-shape p-3 t-1">
-            <i class="fas text-white" :class="result.icon"></i>
-          </div>
-          <div class="pr-3 w-100">
-            <span class="text-light mb-0">{{result.title}}</span>
-            <span class="float-left text-light"><small dir="ltr">{{result.part}} / {{result.total}}</small></span>
-            <div class="progress progress-md mt-2" style="height: 8px">
-              <div class="progress-bar bg-success" role="progressbar" :style="'width: '+result.percent+'%;'" :aria-valuenow="result.percent" aria-valuemin="0" aria-valuemax="100"></div>
+        <nuxt-link :to="result.to" v-for="result in results" :key="result.title">
+          <div class="d-flex align-items-center goal p-3 r-2">
+            <div class="icon icon-shape p-3 t-1">
+              <i class="fas text-white" :class="result.icon"></i>
+            </div>
+            <div class="pr-3 w-100">
+              <span class="text-light mb-0">{{result.title}}</span>
+              <span class="float-left text-light"><small dir="ltr">{{result.part}} / {{result.total}}</small></span>
+              <div class="progress progress-md mt-2" style="height: 8px">
+                <div class="progress-bar bg-success" role="progressbar" :style="'width: '+result.percent+'%;'" :aria-valuenow="result.percent" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
             </div>
           </div>
-        </div>
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -35,6 +37,7 @@ export default {
           total: 100000,
           percent: 5,
           icon: "fa-money-bill",
+          to: '/supermarket/analytics'
         },
         {
           title: "عدد الطلبات",
@@ -42,6 +45,7 @@ export default {
           total: 75,
           percent: 20,
           icon: "fa-cash-register",
+          to: '/supermarket/analytics'
         },
         {
           title: "عدد الزبائن",
@@ -49,6 +53,7 @@ export default {
           total: 1,
           percent: 100,
           icon: "fa-user-friends",
+          to: '/supermarket/analytics'
         },
       ],
     };
